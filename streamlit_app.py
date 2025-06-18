09163176befba39d00c867a9c4395b41bb0aab62
ebwood <timestamp.api@gmail.com>
2025-06-18 20:53:55 +0800

    feat: new version 2.0

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My new Streamlit app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](docs.streamlit.io)"
 )
